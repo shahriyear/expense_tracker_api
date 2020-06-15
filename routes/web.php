f29c475763032cb4656e357d706c38d781cff4ff
fahim.shahiyear <fahim.shahriyear@portonics.com>
2020-06-15 23:40:53 +0600

    undefined route to go 503

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
 
 
 $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
